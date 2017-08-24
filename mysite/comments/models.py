656fcb69984f69ecce2ad4ee9b1fd694e1c92c57
Ngahu <jamaalaraheem@gmail.com>
2017-08-24 17:17:38 +0300

    creating of the fist part dealing with the django generic foreign keys

--- mysite/comments/models.py
+++ mysite/comments/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+from Posts.models import Post
+
+
+class Comment(models.Model):
+    user =  user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1)
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return str(self.user.username)
+
+    def __str__(self):
+        return str(self.user.username)
