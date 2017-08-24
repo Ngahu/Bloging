3c89f29e440346d9ece8ad51bbae22719f50555c
Ngahu <jamaalaraheem@gmail.com>
2017-08-24 05:12:44 +0300

    implementing of a slug field

--- mysite/Posts/models.py
+++ mysite/Posts/models.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
+
+def upload_location(instance,filename):
+    return "%s/%s" %(instance.id,filename)
+
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(null=True, blank=True)
+    slug = models.SlugField(unique=True)
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    image = models.ImageField(upload_to=upload_location,
+                              null=True,
+                              blank=True,
+                              width_field= "width_field",
+                              height_field= "height_field")
     content = models.TextField(max_length=500)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -23,3 +36,26 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+
+#A recursive function to check whether the slug has already been created or not
+
+def create_slug(instance,new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug,qs.first().id)
+        return create_slug(instance,new_slug=new_slug)
+    return slug
+
+
+
+def pre_save_post_receiver(sender,instance,*args,**kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_save_post_receiver,sender=Post)
