4d3fc5846d368dee36046a786983194233afa0d8
Ngahu <jamaalaraheem@gmail.com>
2017-08-24 15:41:40 +0300

    making of the search functionality where one can be able to query and get results from the database

--- mysite/Posts/views.py
+++ mysite/Posts/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm
 from django.http import HttpResponseRedirect,Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.utils import timezone
-
+from django.db.models import Q
 
 def post_create(request):
     if not request.user.is_staff or not request.user.is_superuser:
@@ -43,6 +43,15 @@ def post_list(request):
     queryset_list = Post.objects.active()
     if request.user.is_staff or request.user.is_superuser:
         queryset_list = Post.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+            ).distinct()
     paginator = Paginator(queryset_list, 5) #show 20 blog-posts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
